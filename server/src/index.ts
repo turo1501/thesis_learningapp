4c42550979deb928426f7a193e72d781883eb4a5
Nguyễn Trần Anh Tuấn <tuant32456@gmail.com>
2025-01-31 22:34:09 +0700

    update code for Day 5 6

---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express"
+import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import * as dynamoose from "dynamoose"
+
+dotenv.config()
+
+const isProduction = process.env.NODE_ENV === "production"
+
+if (!isProduction){
+    dynamoose.aws.ddb.local()
+}
+
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({policy :"cross-origin"}))
+app.use(morgan("common"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(cors())
+
+
+app.get("/",(req,res)=>{
+    res.send('hello world')
+})
+
+const port = process.env.PORT|| 3000
+if(!isProduction){
+    app.listen(port,()=>{
+        console.log(`Server running on port ${port}`)
+    })
+}
