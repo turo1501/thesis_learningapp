4896cb7d88bd2ae6a6812e53ca97f341fae48c1c
Nguyễn Trần Anh Tuấn <tuant32456@gmail.com>
2025-02-02 16:58:54 +0700

    #19- Day 7,8_Continue to build Landing page for website

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.pexels.com"], // ✅ Cho phép ảnh từ Pexels
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
--- server/dist/index.js
+++ server/dist/index.js
@@ -43,6 +43,7 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const dynamoose = __importStar(require("dynamoose"));
+const courseRoutes_1 = __importDefault(require("./routes/courseRoutes"));
 dotenv_1.default.config();
 const isProduction = process.env.NODE_ENV === "production";
 if (!isProduction) {
@@ -56,6 +57,7 @@ app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
+app.use("/courses", courseRoutes_1.default);
 app.get("/", (req, res) => {
     res.send('hello world');
 });
