b8a7d46e1bbfc0bbb6bacdc347f6fb88b7a108c2
Nguyễn Trần Anh Tuấn <tuant32456@gmail.com>
2025-02-04 10:43:27 +0700

    #22-Day9 - Config for Clerk Authentication

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers"
+import {ClerkProvider} from "@clerk/nextjs"
 
 const dmSans = DM_Sans({
   subsets: ["latin"],
@@ -21,6 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <ClerkProvider> 
     <html lang="en" className="mdl-js">
       
       <body className={dmSans.className}>
@@ -31,5 +33,7 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
+    </ClerkProvider> 
+
   )
 }
