13988e50e6f505367016e4d23f1c4763cebeeb1d
Nguyễn Trần Anh Tuấn <tuant32456@gmail.com>
2025-02-09 23:15:56 +0700

    #29- Day11 _Update code to fix error for payment service with stripe , build billing page for user and teacher

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ import {
 import courseRoutes from "./routes/courseRoutes";
 import userClerkRoutes from "./routes/userClerkRoutes";
 import transactionRoutes from "./routes/transactionRoutes";
-//import userCourseProgressRoutes from "./routes/userCourseProgressRoutes";
+import userCourseProgressRoutes from "./routes/userCourseProgessRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
 app.use("/courses", courseRoutes);
 app.use("/users/clerk", requireAuth(), userClerkRoutes);
 app.use("/transactions", requireAuth(), transactionRoutes);
-//app.use("/users/course-progress", requireAuth(), userCourseProgressRoutes);
+app.use("/users/course-progress", requireAuth(), userCourseProgressRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3000;
